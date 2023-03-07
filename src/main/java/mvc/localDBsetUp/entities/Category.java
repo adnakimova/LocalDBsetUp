9846415250b7f26a18ad367953494d2587820253
Aidana <akimovadna@gmail.com>
2023-03-07 10:18:45 +0300

    committed

--- src/main/java/mvc/localDBsetUp/entities/Category.java
+++ src/main/java/mvc/localDBsetUp/entities/Category.java
@@ -1,8 +1,53 @@
 package mvc.localDBsetUp.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "categories")
 public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long categoryId;
+	private String categoryName;
+	private String description;
+	
+	public Category() {}
 
+	public Category(long categoryId, String categoryName, String description) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+		this.description = description;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
